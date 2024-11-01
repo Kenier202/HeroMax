d26954e185aa522ec4fc505e7e897a441390bdbc
kenier <kenier202@gmail.com>
2024-11-01 14:48:30 -0500

    feat(heroes): add main route Auth and routes children

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { HeroPagesComponent } from './pages/hero-pages/hero-pages.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPagesComponent } from './pages/list-pages/list-pages.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPagesComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewPageComponent
+      },
+      {
+        path: 'search ',
+        component: SearchPageComponent
+      }
+      ,{
+        path: 'edit/:id',
+        component: NewPageComponent
+      },
+      {
+        path: 'list',
+        component: ListPagesComponent
+      },
+      {
+        path: ':id',
+        component: HeroPagesComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
+      }
+    ],
+  },
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HeroesRoutingModule { }
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,6 +4,8 @@ import { HeroPagesComponent } from './pages/hero-pages/hero-pages.component';
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 import { ListPagesComponent } from './pages/list-pages/list-pages.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
 
 
@@ -12,10 +14,12 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     HeroPagesComponent,
     LayoutPagesComponent,
     ListPagesComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    NewPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HeroesRoutingModule,
   ]
 })
 export class HeroesModule { }
