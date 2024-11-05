49eb958568901729e94e2f04b9d8f5cf1d2423d7
kenier <kenier202@gmail.com>
2024-11-05 11:36:17 -0500

    fea(DashBoard): implement desing dashboard

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPagesComponent } from './pages/hero-pages/hero-pages.component';
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 import { ListPagesComponent } from './pages/list-pages/list-pages.component';
-import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
 import { NewPageComponent } from './pages/new-page/new-page.component';
-
-
+import { NgModule } from '@angular/core';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
     HeroPagesComponent,
     LayoutPagesComponent,
     ListPagesComponent,
+    NewPageComponent,
     SearchPageComponent,
-    NewPageComponent
   ],
   imports: [
     CommonModule,
     HeroesRoutingModule,
+    MaterialModule,
   ]
 })
 export class HeroesModule { }
