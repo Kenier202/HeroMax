a97743fffed446c6856d599c4366c5314ace6826
kenier <kenier202@gmail.com>
2024-11-06 10:18:26 -0500

    feat(HeroPage): load hero information from getHeroById method

--- src/app/heroes/pages/hero-pages/hero-pages.component.ts
+++ src/app/heroes/pages/hero-pages/hero-pages.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-pages',
   templateUrl: './hero-pages.component.html',
   styles: ``
 })
-export class HeroPagesComponent {
+export class HeroPagesComponent implements OnInit{
 
+  public hero?: Heroe;
+  constructor(private heroesService: HeroesService, private activatedRoute: ActivatedRoute,private router: Router) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(switchMap(({id}) => this.heroesService.getHeroById(id)))
+    .subscribe(hero => {
+      if (!hero)  return this.router.navigate(['/heroes/list']);
+
+      this.hero = hero;
+      return;
+    })
+  }
 }
