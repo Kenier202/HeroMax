5bd368ffaad73625f8d035f929aafa9ea1bacbfa
kenier <kenier202@gmail.com>
2024-11-07 11:23:13 -0500

    feat(NewPage): add create new heroe

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Heroe, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
+
+  public heroForm = new FormGroup({
+    superhero:  new FormControl('', {nonNullable: true}),
+    alter_ego:  new FormControl(''),
+    publisher:  new FormControl<Publisher>( Publisher.DCComics),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img:   new FormControl(''),
+  });
 
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router)
+    { }
+
+  get currentHero(): Heroe{
+    const hero = this.heroForm.value as Heroe;
+    return hero;
+  }
+
+  ngOnInit(): void {
+    const urlSegments = this.activatedRoute.snapshot.url.map(segment => segment.path);
+
+    if (!urlSegments.includes('edit')) return;
+  }
+
+
+
+
+  onSubmit(){
+    if(this.heroForm.invalid) return;
+
+    if(this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+      .subscribe(hero =>{} );
+      return;
+    }
+
+
+    this.heroesService.addHero(this.currentHero)
+    .subscribe(hero =>{} );
+    return;
+  }
 }
