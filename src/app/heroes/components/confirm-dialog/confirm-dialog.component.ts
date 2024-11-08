a9e644b96b858bb4d98a14418d8a56e834310da1
kenier <kenier202@gmail.com>
2024-11-08 09:25:37 -0500

    feat(dialog): add confirm dialog delete

---
+++ src/app/heroes/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+})
+export class ConfirmDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ){}
+
+  onNoClick(){
+    this.dialogRef.close(false);
+  }
+
+  onConfirmClick(){
+    this.dialogRef.close(true);
+  }
+}
