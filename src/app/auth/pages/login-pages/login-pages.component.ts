7d8ad51b8372238004310dca887ed40cf45a5a0d
Kenier202 <kenier202@gmail.com>
2024-11-09 13:03:13 -0500

    feat(Auth): add login

--- src/app/auth/pages/login-pages/login-pages.component.ts
+++ src/app/auth/pages/login-pages/login-pages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-pages',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class LoginPagesComponent {
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onLogin(){
+    this.authService.login('john.due@gmail.com', '123456').subscribe( user => {
+      this.router.navigate(['/']);
+
+    });
+  }
 }
