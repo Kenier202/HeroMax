1fee3f48de8ce94b88ab362c1dbe5aa6bcb7b712
kenier <kenier202@gmail.com>
2024-11-12 10:48:25 -0500

    feat(AuthGuard): add checkAuthStatus to redirect to login users logout

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,21 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanMatch, GuardResult, MaybeAsync, Route, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, GuardResult, MaybeAsync, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate,CanMatch {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
+
+  private checkAuthStatus(): boolean | Observable<boolean>{
+    return this.authService.checkAuthentication()
+    .pipe(
+      tap(
+        isAuthenticated => {
+          if (!isAuthenticated){
+            this.router.navigate(['/auth/login']);
+          }
+        }
+      )
+    );
+  }
 
   canMatch(route: Route, segments: UrlSegment[]): MaybeAsync<GuardResult> {
-    console.log("can match");
 
-    console.log(segments, route);
-    return true;
+    return this.checkAuthStatus();
   }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    console.log("can activate");
-    console.log(route, state);
-    return true;
+
+    return this.checkAuthStatus();
   }
 
 }
